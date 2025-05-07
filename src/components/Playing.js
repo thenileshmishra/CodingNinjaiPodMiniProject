5436dadc50acf0bb9f7ed555277cb104aab3902a
thenileshmishra <thenileshmishra@gmail.com>
2025-05-07 18:54:49 +0530

    Playing is converted to function based component

--- src/components/Playing.js
+++ src/components/Playing.js
@@ -1,70 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../css/Playing.css";
 
-class Playing extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentTime: 0
-    };
-    this.intervalId = "";
-  }
+const Playing = ({ songItems, playing, songIndex, audio, songImgUrl }) => {
+  const [currentTime, setCurrentTime] = useState(0);
 
-  // logic for updating the current music playbak
-  componentDidMount() {
-    const { audio } = this.props;
-    this.setState({ currentTime: audio.currentTime });
-    this.intervalId = setInterval(() => {
-      this.setState({ currentTime: this.props.audio.currentTime });
+  useEffect(() => {
+    // Set current time initially
+    setCurrentTime(audio.currentTime);
+
+    // Update time every 100ms
+    const intervalId = setInterval(() => {
+      setCurrentTime(audio.currentTime);
     }, 100);
-  }
 
-  // Clear interval for that same thing
-  componentWillUnmount() {
-    clearInterval(this.intervalId);
-  }
+    // Clear interval on unmount
+    return () => clearInterval(intervalId);
+  }, [audio]);
 
-  // Render playing screen
-  render() {
-    const { songItems, playing, songIndex, audio, songImgUrl } = this.props;
-    var currentTimeRender =
-      Math.floor(this.state.currentTime / 60) +
-      ":" +
-      Math.floor(this.state.currentTime % 60);
-    var durationRender =
-      Math.floor(audio.duration / 60) + ":" + Math.floor(audio.duration % 60);
-    const percentageComplete = {
-      width: (this.state.currentTime / audio.duration) * 100 + "%"
-    };
-    if (durationRender === "NaN:NaN") {
-      durationRender = "0:00";
-    }
-    if (Math.floor(this.state.currentTime % 60 < 10)) {
-      currentTimeRender =
-        Math.floor(this.state.currentTime / 60) +
-        ":0" +
-        Math.floor(this.state.currentTime % 60);
-    }
-    return (
-      <div className="now-playing-container">
-        <div className="song-details">
-          <img src={songImgUrl} alt="songImg"></img>
-          <div>
-            <h6>{songItems[songIndex]}</h6>
-            {playing && <h4 className="play-pause-nav">Playing</h4>}
-            {!playing && <h4 className="play-pause-nav">Paused</h4>}
-          </div>
-        </div>
-        <div className="status">
-          {currentTimeRender}
-          <div id="progress">
-            <div style={percentageComplete} id="progress-bar"></div>
-          </div>
-          {durationRender}
+  // Time formatting
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+  };
+
+  const currentTimeRender = formatTime(currentTime);
+  const durationRender = isNaN(audio.duration) ? "0:00" : formatTime(audio.duration);
+
+  const percentageComplete = {
+    width: (currentTime / audio.duration) * 100 + "%"
+  };
+
+  return (
+    <div className="now-playing-container">
+      <div className="song-details">
+        <img src={songImgUrl} alt="songImg" />
+        <div>
+          <h6>{songItems[songIndex]}</h6>
+          <h4 className="play-pause-nav">{playing ? "Playing" : "Paused"}</h4>
         </div>
       </div>
-    );
-  }
-}
+      <div className="status">
+        {currentTimeRender}
+        <div id="progress">
+          <div style={percentageComplete} id="progress-bar"></div>
+        </div>
+        {durationRender}
+      </div>
+    </div>
+  );
+};
 
 export default Playing;
