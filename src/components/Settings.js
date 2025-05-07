0edc5a9d24f9552e4ca9db92f04d952d28fcc6f6
thenileshmishra <thenileshmishra@gmail.com>
2025-05-07 18:55:53 +0530

    Settings.js is converted to function based component

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import "../css/Settings.css"
+import "../css/Settings.css";
 
- // Renders settings
-class Settings extends React.Component {
-    render() {
-        const {active} = this.props;
-        return (
-            <div className="settings">
-                <h2>Settings</h2>
-                <ul>
-                    {active===0?<li className="active">Themes</li>:<li>Themes</li>}
-                    {active===1?<li className="active">Wheel Color</li>:<li>Wheel Color</li>}
-                    {active===2?<li className="active">Wallpaper</li>:<li>Wallpaper</li>}
-                </ul>
-            </div>
-
-        )
-    }
-
-}
-
+const Settings = ({ active }) => {
+  return (
+    <div className="settings">
+      <h2>Settings</h2>
+      <ul>
+        <li className={active === 0 ? "active" : ""}>Themes</li>
+        <li className={active === 1 ? "active" : ""}>Wheel Color</li>
+        <li className={active === 2 ? "active" : ""}>Wallpaper</li>
+      </ul>
+    </div>
+  );
+};
 
 export default Settings;
