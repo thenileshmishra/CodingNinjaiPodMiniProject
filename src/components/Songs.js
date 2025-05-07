acd291f93239ec47e290eec770657d8a70d27621
thenileshmishra <thenileshmishra@gmail.com>
2025-05-07 18:56:36 +0530

    Songs is converted to function based component

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
 // Renders songs menu
-class Songs extends React.Component {
-    render() {
-        const {songItems,active} = this.props;
-        return (
-            <div className="music">
-                <h3>Songs</h3>
-                <ul>
-                {songItems.map((element, index)=>{
-                            return active===index?<li key={index} className="active">&nbsp;{element}</li>:<li  id="song1" key={index}>&nbsp;{element}</li>
-                        })}
-                </ul>
-            </div>
-
-        )
-    }
-
-}
-
+const Songs = ({ songItems, active }) => {
+  return (
+    <div className="music">
+      <h3>Songs</h3>
+      <ul>
+        {songItems.map((element, index) => (
+          <li
+            key={index}
+            id={active === index ? undefined : "song1"}
+            className={active === index ? "active" : ""}
+          >
+            &nbsp;{element}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Songs;
